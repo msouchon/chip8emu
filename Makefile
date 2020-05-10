aa5a0cf28320630dd3e2e3213234d8aed59a3218
msouchon <marcus_s@live.com.au>
2020-05-10 16:22:22 +1000

    initial graphics

---
+++ Makefile
@@ -0,0 +1,16 @@
+OBJS = main.c chip8.c opcodes.c
+
+CC = gcc
+
+INCLUDE_PATHS = -I C:\msys64\mingw64\include\SDL2
+
+LIBRARY_PATHS = -L C:\msys64\mingw64\lib
+
+COMPILER_FLAGS = -Wall
+
+LINKER_FLAGS = -l mingw32 -l SDL2main -l SDL2
+
+OBJ_NAME = main.exe
+
+all: $(OBJS)
+	$(CC) $(OBJS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <SDL2/SDL.h>
 
 #include "global.h"
 #include "chip8.h"
@@ -20,29 +21,79 @@ void debug_print(chip8* c) {
     }
 }
 
+void to_rgba(uint8_t* in_pix, uint32_t* out_pix) {
+    for (int y = 0; y < Y_SIZE; y++) {
+        for (int x = 0; x < X_SIZE; x++) {
+            if (in_pix[y * X_SIZE + x]) {
+                out_pix[y * X_SIZE + x] = 0xffffff00;
+            }
+            else out_pix[y * X_SIZE + x] = 0;
+        }
+    }
+}
+
 int
 main(int argc, char* argv[]) {
-    chip8* c = chip8_initialize(false);
-    printf("Sucessfully Initialized CHIP8 Struct\n");
 
-    chip8_loadgame(c, argv[1]);
-    printf("Sucessfully Loaded Game\n");
-
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+    SDL_Event event;
+    uint32_t rgba_graphics[X_SIZE * Y_SIZE];
     clock_t start, diff;
+    bool running;
+    chip8* c;
 
-    while (true) {
-        //printf("%04x\n", c->opcode);
+    // Init chip8 core
+    c = chip8_initialize(false);
+    chip8_loadgame(c, argv[1]);
+
+    // Init SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialise, SDL Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    window = SDL_CreateWindow("chip8emu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, X_WINDOW_SIZE, Y_WINDOW_SIZE, SDL_WINDOW_SHOWN);
+    if (!window) {
+	    printf("Window could not be created SDL_Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+	}
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    //SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, X_SIZE, Y_SIZE);
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, X_SIZE, Y_SIZE);
+    
+    // Begin event loop
+    running = true;
+    while (running) {
+        
+
+        while (SDL_PollEvent(&event) != 0) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+            }
+        }
         start = clock();
         chip8_cycle(c);
         if (c->delay_timer > 0) c->delay_timer--;
         if (c->sound_timer > 0) c->sound_timer--;
         diff = start - clock();
         if (c->draw) {
-            debug_print(c);
+            to_rgba(c->graphics, rgba_graphics);
+            SDL_UpdateTexture(texture, NULL, rgba_graphics, X_SIZE * sizeof(uint32_t));
             c->draw = false;
         }
         sleep((1 / CHIP8_CLOCKS_PER_SEC) - diff);
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
